98b69677236de35b7db83acadce321bb9cc2fc00
jasonma1127 <jasonma1127@gmail.com>
2025-12-30 15:43:31 +0800

    feat: enhance ProgressBar with custom colors and animations

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -4,16 +4,34 @@ function ProgressBar(props) {
   const [percentage, setPercentage] = useState(0);
 
   useEffect(() => {
-    setPercentage(props.percentage);
-  }, [props.percentage]);
+    const timer = setTimeout(() => {
+      setPercentage(props.percentage);
+    }, (props.delay || 0) * 1000);
+
+    return () => clearTimeout(timer);
+  }, [props.percentage, props.delay]);
 
   return (
     <div className="progress-bar">
-      <div className="progress-bar-title">{props.title}</div>
+      <div className="progress-bar-title">
+        {props.color && (
+          <span
+            className="language-dot"
+            style={{ backgroundColor: props.color }}
+          ></span>
+        )}
+        {props.title}
+      </div>
       <div className="progress-bar-detail">
         <p>{props.percentage}</p>
         <div className="bar">
-          <div className="bar-percent" style={{ width: `${percentage}` }}></div>
+          <div
+            className="bar-percent"
+            style={{
+              width: `${percentage}`,
+              backgroundColor: props.color || undefined
+            }}
+          ></div>
         </div>
       </div>
     </div>
